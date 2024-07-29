815e3c84e9a683c26db5f9859a5cf4efd6d5f81c
chocolate-funforge <chocolatezlpmn@funforge.io>
2024-07-29 17:58:39 +0800

    feat: use node.fetch instead to avoid body-too-long issue

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 // @ts-check
-const core = require('@actions/core');
-const { HttpClient } = require('@actions/http-client');
-const { delay } = require('./utils/promise');
-const { toJson } = require('./utils/json');
+import {
+  getInput,
+  getMultilineInput,
+  getBooleanInput,
+  info,
+  warning,
+  setFailed,
+  setOutput
+} from '@actions/core';
+// import { HttpClient } from '@actions/http-client';
+import { delay } from './utils/promise';
+import { toJson } from './utils/json';
+import fetch from 'node-fetch';
 
 const main = async () => {
-  const http = new HttpClient();
-
   // Get the inputs
   const inputs = {
-    url: core.getInput('url'),
-    method: core.getInput('method'),
+    url: getInput('url'),
+    method: getInput('method'),
     headers: Object.fromEntries(
       // Turn the array of string headers into an array of key-value pairs
-      core.getMultilineInput('headers').map((header) => header.split(':', 2).map((s) => s.trim()))
+      getMultilineInput('headers').map((header) => header.split(':', 2).map((s) => s.trim()))
     ),
-    body: core.getInput('body'),
-    retryCount: Number(core.getInput('retry-count')),
-    retryDelay: Number(core.getInput('retry-delay')),
-    failOnError: core.getBooleanInput('fail-on-error')
+    body: getInput('body'),
+    retryCount: Number(getInput('retry-count')),
+    retryDelay: Number(getInput('retry-delay')),
+    failOnError: getBooleanInput('fail-on-error')
   };
 
-  core.info(`Inputs: ${toJson(inputs)}`);
+  info(`Inputs: ${toJson(inputs)}`);
 
   let remainingRetryCount = inputs.retryCount;
   while (true) {
     // Make the request
-    const response = await http.request(inputs.method, inputs.url, inputs.body, inputs.headers);
-    const responseSuccess = response.message.statusCode && response.message.statusCode < 400;
+    const response = await fetch(inputs.url, {
+      method: inputs.method,
+      headers: inputs.headers,
+      body: inputs.body
+    });
+    const responseSuccess = response.status && response.status < 400;
 
     // Check for errors
     if (!responseSuccess) {
       // Retry if possible
       if (remainingRetryCount > 0) {
-        core.warning(
-          `Request failed with status code ${response.message.statusCode}. Retries remaining: ${remainingRetryCount}.`
+        warning(
+          `Request failed with status code ${response.status}. Retries remaining: ${remainingRetryCount}.`
         );
 
         if (inputs.retryDelay > 0) {
-          core.info(`Delaying for ${inputs.retryDelay}ms...`);
+          info(`Delaying for ${inputs.retryDelay}ms...`);
           await delay(inputs.retryDelay);
         }
 
@@ -48,38 +59,34 @@ const main = async () => {
       // Otherwise, fail or warn about the error
       else {
         if (inputs.failOnError) {
-          core.setFailed(
-            `Request failed with status code ${response.message.statusCode}. No retries remaining.`
-          );
+          setFailed(`Request failed with status code ${response.status}. No retries remaining.`);
         } else {
-          core.warning(
-            `Request failed with status code ${response.message.statusCode}. No retries remaining.`
-          );
+          warning(`Request failed with status code ${response.status}. No retries remaining.`);
         }
       }
     }
 
     // Read the body
-    const responseBody = await response.readBody();
+    const responseBody = await response.text();
 
     // Set the outputs
     const outputs = {
-      status: response.message.statusCode,
+      status: response.status,
       success: responseSuccess,
-      headers: response.message.headers,
+      headers: response.headers,
       body: responseBody
     };
 
-    core.info(`Outputs: ${toJson(outputs)}`);
+    info(`Outputs: ${toJson(outputs)}`);
 
-    core.setOutput('status', outputs.status);
-    core.setOutput('success', outputs.success);
-    core.setOutput('headers', toJson(outputs.headers));
-    core.setOutput('body', outputs.body);
+    setOutput('status', outputs.status);
+    setOutput('success', outputs.success);
+    setOutput('headers', toJson(outputs.headers));
+    setOutput('body', outputs.body);
 
     // Break out of the retry loop
     break;
   }
 };
 
-main().catch((error) => core.setFailed(error));
+main().catch((error) => setFailed(error));
